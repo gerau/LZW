b8523e1d70fd8f4b3de6cea9147a0038a4500efa
Volodymyr Herashchenko <cholkegn@gmail.com>
2023-12-16 22:54:45 +0200

    added executable class

---
+++ LZW/LZW.cs
@@ -0,0 +1,42 @@
+﻿using LZW.BitStream;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LZW
+{
+    internal class LZW
+    {
+        public List<(byte, int)> dictionary;
+        public LZW()
+        {
+            dictionary = new List<(byte, int)>();
+            for (byte i = 0; i <= byte.MaxValue; i++)
+            {
+                dictionary.Add((i, -1));
+            }
+        }
+
+
+        public void Encode(string inputFile, string outputFile)
+        {
+            int index = -1;
+            string str = "";
+            var writer = new BitStreamWriter(outputFile);
+            if (!File.Exists(inputFile))
+            {
+                throw new Exception($"file {inputFile} doesn't exist!");
+            }
+            var reader = new BinaryReader(new FileStream(inputFile, FileMode.Open));
+            var currentBitLength = 8;
+            try
+            {
+                var c = reader.ReadByte();
+
+            }
+            catch { }
+        }
+    }
+}
---
+++ LZW/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+using LZW.BitStream;
+
+namespace LZW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stream = new BitStream.BitStreamWriter("data.txt");
+
+            byte[] test1 = { 0xe1, 0x01 };
+            byte[] test2 = { 0xee, 0x00 };
+
+            stream.WriteBitSequence(test1, 9);
+            stream.WriteBitSequence(test2, 9);
+
+
+            stream.CloseStream();
+
+            var read = new BitStreamReader("data.txt");
+
+            
+            try
+            {
+                var temp1 = read.ReadBitSequence(11);
+                var temp2 = read.ReadBitSequence(7);
+                foreach (byte b in temp1)
+                {
+                    Console.WriteLine(b.ToString("X"));
+                }
+                foreach (byte b in temp2)
+                {
+                    Console.WriteLine(b.ToString("X"));
+                }
+            }
+            catch
+            {
+            }
+
+        }
+    }
+}
